e046ce03c38d1e2e18d4aed6aa5e56745bdc8b16
Thorsten Schäfer <thorstenschaefer@outlook.com>
2016-05-24 15:51:11 -0300

    Fixed build issues causes by Angular CLI bug 847

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -8,17 +8,17 @@ module.exports = function(defaults) {
       // angular and its dependencies
       'systemjs/dist/system-polyfills.js',
       'systemjs/dist/system.src.js',
-      'zone.js/dist/*.js',
+      'zone.js/dist/**/*.+(js|js.map)',
       'es6-shim/es6-shim.js',
-      'reflect-metadata/*.js',
-      'rxjs/**/*.js',
-      '@angular/**/*.js',
+      'reflect-metadata/**/*.+(js|js.map)',
+      'rxjs/**/*.+(js|js.map)',
+      '@angular/**/*.+(js|js.map)',
       // ng2-charts
       'chart.js/dist/Chart.bundle.min.js',
       'ng2-charts/**/*.js',
       // firebase
-      'angularfire2/**/*.js',
-      'firebase/lib/*.js'      
+      'angularfire2/**/*.+(js|js.map)',
+      'firebase/lib/*.+(js|js.map)'      
     ]
   });
 };
